a078a639c853c9bba2982cf1ed2c7fddc80235fa
zoupeiyang <zoupeiyang@gmail.com>
2015-07-08 17:12:18 +0800

    add some

--- Application/Admin/Controller/MenuController.class.php
+++ Application/Admin/Controller/MenuController.class.php
@@ -2,8 +2,20 @@
 namespace Admin\Controller;
 use Think\Controller;
 class MenuController extends Controller {
+    private $parent_title="系统设置";
     public function index(){
 
+        $menus = M('menu')->select();
+        $this->assign('list',$menus);
+        $this->assign('parent_title',$this->parent_title);
+        $this->assign('current_title',"菜单管理");
+        $this->display();
+    }
+
+    public function add_menu()
+    {
+        $this->assign('parent_title',$this->parent_title);
+        $this->assign('current_title',"添加菜单");
         $this->display();
     }
 }
--- Document/database.sql
+++ Document/database.sql
@@ -40,41 +40,33 @@ create table `users`(
   PRIMARY KEY  (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+create table `menu`(
+`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+`menu_type` tinyint(1) not null comment '菜单类型,1表示一级菜单,2表示二级菜单,3表示三级菜单',
+`parent_id` smallint(5) not null comment '菜单父类id,一级菜单的父类id为0',
+`menu_name` varchar(25) not null comment '菜单名称',
+`module_name` varchar(30) comment '模块名称',
+`controller_name` varchar(30) comment '控制器名称',
+`action_name` varchar(30) comment '操作名称',
+`param` varchar(100) comment '参数',
+`link_url` varchar(200) comment '自定义URL',
+`create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+`update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
+`order_id`smallint(5) unsigned NOT NULL DEFAULT '0'comment '排序号，越大越靠前',
+`is_show` tinyint(1) NOT NULL DEFAULT '1' comment '是否显示,1表示显示,0表示不显示',
+primary key (`id`)
 
-CREATE TABLE `menu` (
-  `ID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `MenuName` varchar(15) NOT NULL,
-  `BkParentID` smallint(5) unsigned NOT NULL,
-  `GnParentID` smallint(5) unsigned NOT NULL,
-  `LinkUrl` varchar(80) DEFAULT NULL,
-  `OrderID` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `IsShow` tinyint(1) NOT NULL DEFAULT '1',
-  `Module` varchar(20) Default null,
-  `Controller` varchar(20) DEFAULT NULL,
-  `Action` varchar(20) DEFAULT NULL,
-  `Param` varchar(20) default null,
-  `AddTime` datetime DEFAULT NULL,
-  `AddMan` varchar(15) DEFAULT 'Admin',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+)
+ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+
 
 CREATE TABLE `menu_action` (
-  `ID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `ActionName` varchar(15) DEFAULT NULL,
-  `ParentID` smallint(5) unsigned DEFAULT NULL,
-  `Controller` varchar(20) DEFAULT NULL,
-  `Action` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `action_name` varchar(15) DEFAULT NULL,
+  `menu_id` smallint(5) unsigned DEFAULT NULL comment '归属的菜单id',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `menu_group` (
-  `ID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `GroupName` varchar(15) NOT NULL,
-  `ParentID` smallint(5) unsigned DEFAULT NULL,
-  `OrderID` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `IsShow` tinyint(1) NOT NULL DEFAULT '1',
-  `AddTime` datetime DEFAULT NULL,
-  `AddMan` varchar(15) DEFAULT 'Admin',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8;
+
 
